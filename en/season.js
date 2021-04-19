640f4000251e0a3a555bddaf6e61b3cf95e837b9
rain-band <eternalseason@hotmail.com>
2021-04-20 00:49:58 +0900

    英語ページを追加

---
+++ en/season.js
@@ -0,0 +1,43 @@
+var flake_count = 50;
+var flake_color = "#ffffff";
+var image = "../image/snow.png";
+
+var dt = new Date();
+var month = dt.getMonth();
+var today = dt.getDate();
+
+if (month == 12) {
+    ;
+} else if (month >= 2) {
+    flake_count = 29;
+    flake_color = "#eac2d0";
+    image = "../image/sakura.png";
+};
+
+
+jQuery(document).ready(function(){
+    jQuery(document).snowfall({
+    // 雪の量 (数値)
+    flakeCount : flake_count,
+    // 色 (RGB) (e.g., sakura: #eac2d0)
+    flakeColor : flake_color,
+    // z-indexの値
+    //flakeIndex : "888",
+    // 最小サイズ (数値)
+    minSize : 2.5,
+    // 最大サイズ(数値)
+    maxSize : 10,
+    // 最低速度(数値)
+    minSpeed : 1,
+    // 最高速度(数値)
+    maxSpeed : 5,
+    // 雪の形を丸にする(boolean)
+    // round : true,
+    // 影をつける(boolean)
+    // shadow : false,
+    // 要素に積もらせる場合
+    // collection : "div.bottom",
+    // オリジナル画像を使用する場合
+    image : image
+    })
+});
