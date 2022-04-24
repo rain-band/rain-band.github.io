4cf5bb9f036827228fc1c371e1276bb72bdc6f7d
rain-band <eternalseason@hotmail.com>
2022-04-24 11:46:26 +0900

    Update 2022.04.24

--- en/season.js
+++ en/season.js
@@ -1,15 +1,24 @@
 var flake_count = 50;
 var flake_color = "#ffffff";
+var minSize = 2.5;
+var maxSize = 10;
+var minSpeed = 1;
+var maxSpeed = 5;
 var image = "../image/snow.png";
 
 var dt = new Date();
 var month = dt.getMonth();
 var today = dt.getDate();
 
-if (month >= 10) {
-    ;
+if (10 > month >= 4) {
+    flake_count = 300;
+    minSize = 0.1;
+    maxSize = 2.5;
+    minSpeed = 12;
+    maxSpeed = 15;
+    image = "../image/rain.png"
 } else if (month >= 2) {
-    flake_count = 29;
+    flake_count = 42;
     flake_color = "#eac2d0";
     image = "../image/sakura.png";
 };
@@ -24,13 +33,13 @@ jQuery(document).ready(function(){
     // z-indexの値
     //flakeIndex : "888",
     // 最小サイズ (数値)
-    minSize : 2.5,
+    minSize : minSize,
     // 最大サイズ(数値)
-    maxSize : 10,
+    maxSize : maxSize,
     // 最低速度(数値)
-    minSpeed : 1,
+    minSpeed : minSpeed,
     // 最高速度(数値)
-    maxSpeed : 5,
+    maxSpeed : maxSpeed,
     // 雪の形を丸にする(boolean)
     // round : true,
     // 影をつける(boolean)
@@ -41,3 +50,4 @@ jQuery(document).ready(function(){
     image : image
     })
 });
+
